31a466c961227b7ead8f8333442a78c21dad8c15
vishnu <redvishnu@gmail.com>
2024-11-17 13:16:38 +0530

    raw

---
+++ NOV/ex_005_python_basics/lab3escapesequence.py
@@ -0,0 +1,8 @@
+print("hellow\nworld")
+print("hellow\tworld")
+print("hellow\bworld")
+c = r'c:\vishnu\n.txt' #this concept is called as raw string concept
+print(c)
+d = r"c:\vishnu\t.txt"
+print(d)
+
